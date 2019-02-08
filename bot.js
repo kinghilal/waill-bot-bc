3fe750f9b4a0114ea65c03d7677c1d51170fea49
kinghilal <46085303+kinghilal@users.noreply.github.com>
2019-02-08 17:15:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
-1const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("538296844103450644")
+let channel =     client.channels.get("538357433643565078")
 setInterval(function() {
 channel.send(`test test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test test`);
 }, 30)
 })
 
-   client.login("NTM5ODIwODc4NzU0NDgwMTQ3.Dz8VgQ.srMYBLcRwE5TQepvzq--iH25Kp8");
+   client.login('NTQzNDIzMjk0NjYxOTg0MjU2.Dz8cYw._SMq9HvPWXk_VBQMLkZZf6xZbFQ');
    
